a6a084f0315562934ff56d6d0a9e5717187ec21f
unprosaiclabyrinth <hdongr2@uic.edu>
2025-02-22 16:36:19 -0600

    Run 10k trials using multithreading in Julia

---
+++ eval10k.jl
@@ -0,0 +1,19 @@
+using Base.Threads
+
+println("Running wumpus simulation 10000 times and compiling scores...")
+
+# Run trials and compile scores in parallel
+Threads.@sync @threads for i in 1:100 # 100 iterations
+    num_trials = 100 # 100 trials per iteration
+    outfile = "jltlv_wumpus_eval$i.txt"
+    infile = "jltlv_wumpus_out$i.txt"
+    run(`./wumpus_eval.sh -n $num_trials -i $infile -o $outfile`)
+    println("$(threadid()) -> $i")
+end
+
+# Compile all scores
+run(`zsh -c "cat jltlv_wumpus_eval*.txt > wumpus_eval.txt"`)
+println("Scores written to wumpus_eval.txt")
+
+# Clean up
+run(`zsh -c "rm -f jltlv_wumpus_{out,eval}*.txt"`)
