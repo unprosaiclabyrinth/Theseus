c6ff39336b50139872af4e41d2c0fe20d93616dd
unprosaiclabyrinth <hdongr2@uic.edu>
2025-02-22 16:29:10 -0600

    Add input_file option and suppress dump

--- wumpus_eval.sh
+++ wumpus_eval.sh
@@ -2,10 +2,11 @@
 
 # Function to display usage
 usage() {
-    echo "usage: $0 [-n <num_trials>] [-o <output_file>] [-h]"
+    echo "usage: $0 [-n <num_trials>] [-o <output_file>] [-i <input_file>] [-h]"
     echo "Options:"
     echo "  -n <num_trials>  Number of times to run the simulation."
     echo "  -o <output_file> Output file to save the scores."
+    echo "  -i <input_file>  Input file to read the score. The simulator will write to this file."
     echo "  -h               Display this help message and exit."
     exit 1
 }
@@ -13,9 +14,10 @@ usage() {
 # Initialize variables
 number=10
 output_file="wumpus_eval.txt"
+input_file="wumpus_out.txt"
 
 # Parse command-line options
-while getopts ":n:o:h" opt; do
+while getopts ":n:o:i:h" opt; do
     case $opt in
         n)
             number=$OPTARG
@@ -23,6 +25,10 @@ while getopts ":n:o:h" opt; do
         o)
             output_file=$OPTARG
             ;;
+
+        i) input_file=$OPTARG
+            ;;
+
         h)
             usage
             ;;
@@ -37,14 +43,14 @@ while getopts ":n:o:h" opt; do
     esac
 done
 
-echo "Running wumpus simulation $number times and compiling scores..."
+# echo "Running wumpus simulation $number times and compiling scores..."
 
 for((i=0;i<number;++i)); do
-    scala run -cp target --main-class WorldApplication -- -n false -a false -s 200 > /dev/null;
-    tail -n2 wumpus_out.txt | head -n1 | cut -d ':' -f 2 | awk '{print $1}';
+    scala run -cp target --main-class WorldApplication -- -n false -a false -s 200 -f "$input_file" > /dev/null;
+    tail -n2 "$input_file" | head -n1 | cut -d ':' -f 2 | awk '{print $1}';
 done > "$output_file"
 
-echo "Scores written to \"$output_file\""
+# echo "Scores written to \"$output_file\""
 
-avg=$(echo "$(tr '\n' '+' < "$output_file" | sed 's/+$/\n/' | bc)/$number" | bc)
-echo "Average score over $number trials was $avg"
+# avg=$(echo "$(tr '\n' '+' < "$output_file" | sed 's/+$/\n/' | bc)/$number" | bc)
+# echo "Average score over $number trials was $avg"
