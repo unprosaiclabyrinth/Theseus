20a24716b75196ab895c7dc3493ecf606b24692e
unprosaiclabyrinth <hdongr2@uic.edu>
2025-03-01 00:39:42 -0600

    Lay groundwork for utility-based agent

---
+++ src/scala/UtilityBasedAgent.scala
@@ -0,0 +1,17 @@
+/*
+ * Agent function for a utility-based agent in the wumpus environment
+ * f_UBA: Ω* -> A
+ * 
+ * Project 3 - Utility-Based Agent
+ * for CS 511: Artificial Intelligence II, Spring 2025
+ * at the University of Illinois Chicago
+ * 
+ */
+import scala.collection.mutable
+
+object UtilityBasedAgent extends AgentFunctionImpl:
+  override def reset(): Unit =
+    ???
+  
+  override def process(tp: TransferPercept): Int =
+    ???
