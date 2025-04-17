319bbb796d8e84d1f866ad4da9d7e1b4e5ace87d
unprosaiclabyrinth <hdongr2@uic.edu>
2025-04-16 19:07:36 -0500

    Ban all moves but GRAB if glitter

--- src/scala/UtilityBasedAgent.scala
+++ src/scala/UtilityBasedAgent.scala
@@ -125,14 +125,14 @@ object UtilityBasedAgent extends AgentFunctionImpl:
         val validRight = !outOfBounds(agentOrientation.rightFrom(agentPosition))
         val validBack = !outOfBounds(agentOrientation.backFrom(agentPosition))
         val inclusionCondition: Map[Move, Boolean] = Move.values.map(m => m -> (m match {
-          case Move.GoForward => validFront
-          case Move.GoLeft => validLeft
-          case Move.GoRight => validRight
-          case Move.GoBack => validBack
-          case Move.Shoot => hasArrow && validFront && o.stench
-          case Move.ShootLeft => hasArrow && validLeft && o.stench
-          case Move.ShootRight => hasArrow && validRight && o.stench
-          case Move.NoOp => true
+          case Move.GoForward => validFront && !o.glitter
+          case Move.GoLeft => validLeft && !o.glitter
+          case Move.GoRight => validRight && !o.glitter
+          case Move.GoBack => validBack && !o.glitter
+          case Move.Shoot => hasArrow && validFront && o.stench && !o.glitter
+          case Move.ShootLeft => hasArrow && validLeft && o.stench && !o.glitter
+          case Move.ShootRight => hasArrow && validRight && o.stench && !o.glitter
+          case Move.NoOp => !o.glitter
           case Move.Grab => o.glitter
         })).toMap
         Move.values.toSet filter inclusionCondition
@@ -290,7 +290,7 @@ object UtilityBasedAgent extends AgentFunctionImpl:
             memo += (b -> utility)
             Node(b, utility, successors, d)
         }
-      else if (root.depth == TIME_HORIZON) || b.isTerminal then // leaf action node
+      else if d == TIME_HORIZON || (b isTerminal) then // leaf action node
         Node(b, b.lastUpdate.asInstanceOf[Move].reward + b.eval, Map.empty, d)
       else // interior action node
         val successors: Map[Move | Percept4, Node] =
